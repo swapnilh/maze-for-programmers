a19a16e1defae27fd3e6f943f8bf9cda44129469
Swapnil Haria <swapnilster@gmail.com>
2019-06-24 16:59:43 -0500

    Making num_rows_/cols_ be external dims

--- maze.cpp
+++ maze.cpp
@@ -2,15 +2,15 @@
 #include "maze.hpp"
 
 Cell Maze::GetWall(Cell cell, Direction dir) {
-	int actual_row = 2*cell.row + 1;
-	int actual_col = 2*cell.col + 1;
-
-	if (actual_row >= num_rows_ || actual_row < 0
-			|| actual_col >= num_cols_ || actual_col < 0) {
+	if (cell.row >= num_rows_ || cell.row < 0
+			|| cell.col >= num_cols_ || cell.col < 0) {
 		std::cerr << "Cell dimensions incorrect: ["
 			<< cell.row  << "," << cell.col << "]" << std::endl;
 		return  {-1, -1};
 	}
+
+	int actual_row = 2*cell.row + 1;
+	int actual_col = 2*cell.col + 1;
 
 	switch (dir) {
 		case Direction::North:
@@ -35,8 +35,8 @@ Cell Maze::GetWall(Cell cell, Direction dir) {
 }
 
 void Maze::Display() {
-  for (int row = 0; row < num_rows_; row++) {
-    for (int col = 0; col < num_cols_; col++) {
+  for (int row = 0; row < 2*num_rows_+1; row++) {
+    for (int col = 0; col < 2*num_cols_+1; col++) {
       char symbol = (cells_[row][col] == false) ? ' ' : '#';
       std::cout << symbol << symbol;
     }
@@ -54,8 +54,8 @@ bool Maze::CloseWall (int row, int col, Direction dir) {
 	}
 
 	// If cell border is also the external border of the maze, return false.
-	if (wall.row >= num_rows_ || wall.row <= 0
-			|| wall.col >= num_cols_ || wall.col <= 0) {
+	if (wall.row >= 2*num_rows_+1 || wall.row <= 0
+			|| wall.col >= 2*num_cols_+1 || wall.col <= 0) {
 		return false;
 	}
 
@@ -74,8 +74,8 @@ bool Maze::OpenWall (int row, int col, Direction dir) {
 	}
 
 	// If cell border is also the external border of the maze, return false.
-	if (wall.row >= num_rows_ || wall.row <= 0
-			|| wall.col >= num_cols_ || wall.col <= 0) {
+	if (wall.row >= 2*num_rows_+1 || wall.row <= 0
+			|| wall.col >= 2*num_cols_+1 || wall.col <= 0) {
 		return false;
 	}
 
--- maze.hpp
+++ maze.hpp
@@ -21,8 +21,12 @@ struct Cell {
 // The M x N maze is represented as 2M+1 x 2N+1 as walls are stored as cells.
 // All odd indices are real cells, even indices are walls (open or closed).
 class Maze {
+
+	// Stores the number of rows and the cols of the original maze
+	// and does not include the count of walls.
   int num_rows_;
   int num_cols_;
+
   // true = closed, false = open.
   // odd indices cannot be closed as they are real cells. 
   // TODO: store only walls and not cells.
@@ -34,24 +38,33 @@ class Maze {
   
 public:
   Maze(int num_rows, int num_cols) :
-    num_rows_(2*num_rows+1), num_cols_(2*num_cols+1),
+    num_rows_(num_rows), num_cols_(num_cols),
     cells_(2*num_rows+1, std::vector<bool>(2*num_cols+1, false)) {
 
     // Closing all east-west walls.
-    for (int row = 0; row < num_rows_; row+=2) {
-      for (int col = 0; col < num_cols_; col++) {
+    for (int row = 0; row < 2*num_rows_+1; row+=2) {
+      for (int col = 0; col < 2*num_cols_+1; col++) {
         cells_[row][col] = true;
       }
     }
     // Closing all north-south walls.
-    for (int col = 0; col < num_cols_; col+=2) {
-      for (int row = 0; row < num_rows_; row++) {
+    for (int col = 0; col < 2*num_cols_+1; col+=2) {
+      for (int row = 0; row < 2*num_rows_+1; row++) {
         cells_[row][col] = true;
       }
     }
 
   }
 	
+	int GetNumRows () {
+		return num_rows_;
+	}
+
+	// Returns the number of cols
+	int GetNumCols () {
+		return num_cols_;
+	}
+
 	// ASCII-based display of the maze	
   void Display();
 
