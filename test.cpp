c4c8fb22a6162b56246b9f0c81e1f47f4f84e641
Swapnil Haria <swapnilster@gmail.com>
2019-06-27 00:25:51 -0500

    Rewriting test.cpp

--- test.cpp
+++ test.cpp
@@ -1,65 +1,41 @@
 #include <iostream>
 #include <cassert>
 #include "maze.hpp"
+#include "maze_generator.hpp"
 #include "binary_tree.hpp"
 #include "sidewinder.hpp"
 #include "dijkstra.hpp"
 #include "aldous_broder.hpp"
 #include "hunt_and_kill.hpp"
+#include "recursive_backtrack.hpp"
+
+constexpr auto GRID_DIM = 6;
 
 int main () {
-  Maze basicTwo(2, 2);
-  //basicFive.DebugDisplay();
-  basicTwo.Display();
 
-  Maze basicFive(10, 10);
+  Maze test_maze(GRID_DIM, GRID_DIM);
+	std::vector<MazeGenerator*> generators;
+	generators.push_back(new BinaryTreeGenerator(test_maze));
+	generators.push_back(new SidewinderGenerator(test_maze));
+	generators.push_back(new AldousBroderGenerator(test_maze));
+	generators.push_back(new HuntAndKillGenerator(test_maze));
+	generators.push_back(new RecursiveBacktrackGenerator(test_maze));
+	DijkstraSolver dj(test_maze);
 
-  basicFive.Display();
-	std::cout << "Generating a perfect maze using binary_tree algorithm"
-					  << std::endl;
-	BinaryTreeGenerator bt(basicFive);
-	bt.Generate();
-  basicFive.Display();
-	std::cout << "Deadend Cells:" << basicFive.GetDeadendCells().size()
-					  << std::endl;
-
-	std::cout << "Resetting the maze" << std::endl;
-  basicFive.Reset();
-
-	std::cout << "Generating a perfect maze using sidewinder algorithm"
-					  << std::endl;
-	SidewinderGenerator sw(basicFive);
-	sw.Generate();
-  basicFive.Display();
-	std::cout << "Deadend Cells:" << basicFive.GetDeadendCells().size()
-					  << std::endl;
-
-	std::cout << "Resetting the maze" << std::endl;
-  basicFive.Reset();
-
-	std::cout << "Generating a perfect maze using aldous-broder algorithm"
-					  << std::endl;
-	AldousBroderGenerator ab(basicFive);
-	ab.Generate();
-  basicFive.Display();
-	std::cout << "Deadend Cells:" << basicFive.GetDeadendCells().size()
-					  << std::endl;
-
-	std::cout << "Resetting the maze" << std::endl;
-  basicFive.Reset();
-
-	std::cout << "Generating a perfect maze using hunt-and-kill algorithm"
-					  << std::endl;
-	HuntAndKillGenerator hk(basicFive);
-	hk.Generate();
-  basicFive.Display();
-	std::cout << "Deadend Cells:" << basicFive.GetDeadendCells().size()
-					  << std::endl;
-
-	std::cout << "Solving the maze using djikstra's algorithm" << std::endl;
-	DijkstraSolver dj(basicFive);
-	dj.Reset();
-	dj.Solve();
-
+	for (auto generator : generators) {
+		std::cout << "Generating a perfect maze using "
+					  <<  generator->GetName() << " algorithm" << std::endl;
+		generator->Generate();
+		//test_maze.Display();	
+		std::cout << "Deadend Cells:" << test_maze.GetDeadendCells().size()
+						  << std::endl;
+		
+		std::cout << "Solving the maze using djikstra's algorithm" << std::endl;
+		dj.Reset();
+		dj.Solve();
+		
+		std::cout << "Resetting the maze" << std::endl;
+		test_maze.Reset();
+	}
   return 0;
 }
