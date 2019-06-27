484cd45fd744dbaa8fd3376475db836068affe6f
Swapnil Haria <swapnilster@gmail.com>
2019-06-26 23:35:17 -0500

    Fixing missing reset

--- test.cpp
+++ test.cpp
@@ -13,13 +13,15 @@ int main () {
   basicTwo.Display();
 
   Maze basicFive(10, 10);
-/*
+
   basicFive.Display();
 	std::cout << "Generating a perfect maze using binary_tree algorithm"
 					  << std::endl;
 	BinaryTreeCreator bt(basicFive);
 	bt.Generate();
   basicFive.Display();
+	std::cout << "Deadend Cells:" << basicFive.GetDeadendCells().size()
+					  << std::endl;
 
 	std::cout << "Resetting the maze" << std::endl;
   basicFive.Reset();
@@ -29,10 +31,8 @@ int main () {
 	SidewinderCreator sw(basicFive);
 	sw.Generate();
   basicFive.Display();
-
-	std::cout << "Solving the maze using djikstra's algorithm" << std::endl;
-	DijkstraSolver dj(basicFive);
-	dj.Solve();
+	std::cout << "Deadend Cells:" << basicFive.GetDeadendCells().size()
+					  << std::endl;
 
 	std::cout << "Resetting the maze" << std::endl;
   basicFive.Reset();
@@ -42,18 +42,24 @@ int main () {
 	AldousBroderCreator ab(basicFive);
 	ab.Generate();
   basicFive.Display();
-*/
+	std::cout << "Deadend Cells:" << basicFive.GetDeadendCells().size()
+					  << std::endl;
+
+	std::cout << "Resetting the maze" << std::endl;
+  basicFive.Reset();
+
 	std::cout << "Generating a perfect maze using hunt-and-kill algorithm"
 					  << std::endl;
 	HuntAndKillCreator hk(basicFive);
 	hk.Generate();
   basicFive.Display();
+	std::cout << "Deadend Cells:" << basicFive.GetDeadendCells().size()
+					  << std::endl;
 
 	std::cout << "Solving the maze using djikstra's algorithm" << std::endl;
 	DijkstraSolver dj(basicFive);
 	dj.Reset();
 	dj.Solve();
-
 
   return 0;
 }
