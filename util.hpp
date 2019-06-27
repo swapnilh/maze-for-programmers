70f8325b3a5c2e604f5a4c7c76e3e2e0cc356ef6
Swapnil Haria <swapnilster@gmail.com>
2019-06-26 23:12:48 -0500

    Adding logging capability

--- util.hpp
+++ util.hpp
@@ -1,11 +1,21 @@
 #ifndef UTIL_HPP
 #define UTIL_HPP
 
+#include <iostream>
+
 enum class Direction {
 	North,
 	East,
 	West,
 	South
 };
+
+#ifdef DEBUG
+#define NOTE(...) {fprintf(stderr, __VA_ARGS__);}
+#define LOG(M, ...) fprintf(stderr, "%s (%s:%d) " M "\n", __func__, __FILE__, __LINE__, __VA_ARGS__);
+#else
+#define NOTE(M, ...) do {} while(0)
+#define LOG(M, ...) do {} while(0)
+#endif
 
 #endif // UTIL_HPP
