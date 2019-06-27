e090c9896e6998e22b267bff622f3b196b89e020
Swapnil Haria <swapnilster@gmail.com>
2019-06-27 00:25:09 -0500

    Changing name of maze in deadend_stats

--- deadend_stats.cpp
+++ deadend_stats.cpp
@@ -8,25 +8,27 @@
 #include "dijkstra.hpp"
 #include "aldous_broder.hpp"
 #include "hunt_and_kill.hpp"
+#include "recursive_backtrack.hpp"
 
 constexpr auto GRID_DIM = 20;
 constexpr auto ITERATIONS = 100;
 
 int main () {
 
-  Maze basicTwenty(GRID_DIM, GRID_DIM);
+  Maze test_maze(GRID_DIM, GRID_DIM);
 	std::vector<MazeGenerator*> generators;
-	generators.push_back(new BinaryTreeGenerator(basicTwenty));
-	generators.push_back(new SidewinderGenerator(basicTwenty));
-	generators.push_back(new AldousBroderGenerator(basicTwenty));
-	generators.push_back(new HuntAndKillGenerator(basicTwenty));
+	generators.push_back(new BinaryTreeGenerator(test_maze));
+	generators.push_back(new SidewinderGenerator(test_maze));
+	generators.push_back(new AldousBroderGenerator(test_maze));
+	generators.push_back(new HuntAndKillGenerator(test_maze));
+	generators.push_back(new RecursiveBacktrackGenerator(test_maze));
 
 	for (auto generator : generators) {
 		int deadend_count = 0;
 		for (int iter = 0; iter < ITERATIONS; iter++) {
 			generator->Generate();
-			deadend_count += basicTwenty.GetDeadendCells().size();
-			basicTwenty.Reset();
+			deadend_count += test_maze.GetDeadendCells().size();
+			test_maze.Reset();
 		}
 		auto avg_deadend_count = deadend_count / ITERATIONS;
 		std::cout << generator->GetName() << " : " << avg_deadend_count << "/"
